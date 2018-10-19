194576cddaa0f519dcbf18b2521f47aef5db4e4a
Quinton Bolt <qbolt@cooksys.com>
2018-10-19 09:49:32 -0500

    Created client/server programs; client creates/marshals quoteRequest to server; server reads request and fetches stock quote from api; formats result to client

--- src/main/java/com/cooksys/client/Client.java
+++ src/main/java/com/cooksys/client/Client.java
@@ -1,4 +1,55 @@
 package com.cooksys.client;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.*;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Client {
+
+    public static void main(String[] args) {
+
+        // List of fields that the client is requesting on a quote
+        Set<QuoteField> fields = new HashSet<>(Collections.singletonList((QuoteField.LATEST_PRICE)));
+
+        // Stock ticker symbols client is requesting
+        Set<String> symbols = new HashSet<>(Arrays.asList("NIO", "TWTR"));
+
+        // Encapsulating request object
+        QuoteRequest request = new QuoteRequest(fields, symbols);
+
+        try {
+            Socket socket = new Socket("localhost", 3000);
+
+            JAXBContext context = JAXBContext.newInstance(QuoteField.class, QuoteRequest.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            // Marshal to socket
+            marshaller.marshal(request, System.out);
+            marshaller.marshal(request, socket.getOutputStream());
+
+            socket.getOutputStream().flush();
+
+            // Get response
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String message;
+            while (socket.isConnected() && (message = in.readLine()) != null) {
+                System.out.println(message);
+            }
+
+//            Thread t = new Thread(new QuoteReader(socket));
+//            t.start();
+        } catch (JAXBException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/main/java/com/cooksys/server/Server.java
+++ src/main/java/com/cooksys/server/Server.java
@@ -1,4 +1,87 @@
 package com.cooksys.server;
 
+import pl.zankowski.iextrading4j.api.stocks.BatchStocks;
+import pl.zankowski.iextrading4j.api.stocks.Quote;
+import pl.zankowski.iextrading4j.client.IEXTradingClient;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.QuoteRequestBuilder;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Server {
+
+    public static void main(String[] args) {
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(QuoteField.class, QuoteRequest.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            ServerSocket server = new ServerSocket(3000);
+            Socket client = server.accept();
+            System.out.println("Got request from client");
+
+            QuoteRequest request = (QuoteRequest) unmarshaller.unmarshal(client.getInputStream());
+            System.out.println("Unmarshalled request");
+
+            IEXTradingClient iexTradingClient = IEXTradingClient.create();
+            Set<Quote> quotes = new HashSet<>();
+
+            for (String symbol : request.getSymbols()) {
+                quotes.add(iexTradingClient.executeRequest(new QuoteRequestBuilder()
+                        .withSymbol(symbol)
+                        .build()));
+            }
+
+            StringBuilder response = new StringBuilder();
+            for (Quote quote : quotes) {
+                response.append("\n").append(quote.getSymbol()).append(" -");
+                for (QuoteField field : request.getFields()) {
+                    switch (field) {
+                        case HIGH:
+                            response.append(" High: ").append(quote.getHigh());
+                            break;
+                        case LOW:
+                            response.append(" Low: ").append(quote.getLow());
+                            break;
+                        case CLOSE:
+                            response.append(" Close: ").append(quote.getClose());
+                            break;
+                        case OPEN:
+                            response.append(" Open: ").append(quote.getOpen());
+                            break;
+                        case CHANGE_PERCENT:
+                            response.append(" Change Percent: ").append(quote.getChangePercent());
+                            break;
+                        case CHANGE:
+                            response.append(" Change: ").append(quote.getChange());
+                            break;
+                        case LATEST_PRICE:
+                            response.append(" Latest Price: ").append(quote.getLatestPrice());
+                            break;
+                    }
+                }
+            }
+
+            System.out.println(response);
+
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            out.write(response.toString());
+            out.flush();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
